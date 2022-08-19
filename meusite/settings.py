84427f9854d94094822117d3a1bde989e189b01b
renanunreal <renanunreal@gmail.com>
2022-08-19 16:37:38 -0300

    teste

--- meusite/settings.py
+++ meusite/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'meusite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'static/paginas/')],
+        'DIRS': [os.path.join(BASE_DIR, 'Statics/paginas/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Statics/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'Statics/')
 ]
 
 # Default primary key field type
