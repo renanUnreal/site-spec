f3fbd2483acc6ee395ac20166fde78cbfc334dc2
renanunreal <renanunreal@gmail.com>
2022-08-22 20:11:03 -0300

    'resete'

--- produtos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.produtos, name = 'produtos'),
-    path('<int:produto_id>', views.ver_produto, name = 'ver_produto'),
-    path('busca', views.busca, name='busca')
-]
